05aa9b3396e26e7b901c8977d537df52c53f9fdc
Lucas Correa <lucascorreanasarioprogramador@gmail.com>
2023-06-27 00:38:50 -0300

    feat: atividade consumo em litros do seu carro

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function soma(item, item2){
-    return item + item2;
+const consumoCar = 12;
+
+function gastoGasolina(tempoGasto, velocidadeMedia) {
+    return (tempoGasto * velocidadeMedia) / consumoCar;
 }
 
-
-console.log("Soma" , soma(5, 6));
+console.log(gastoGasolina(10, 85).toFixed(3));
+console.log(gastoGasolina(2, 92).toFixed(3));
+console.log(gastoGasolina(22, 67).toFixed(3));
