cae9d0784c79b73b7d2b3daa97a8d34fbddcfcd2
Lucas Correa <lucascorreanasarioprogramador@gmail.com>
2023-06-26 21:49:43 -0300

    feat:sobre  array

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-let idade = 18;
+const idadeLucas = 18;
+const idadeJoao = 14;
+const idadeJonny = 45;
 
-do{
-    console.log("Passou");
-  
+const idades = [18, 14, 45];
+
+for(let i = 0; i< idades.length; i++){
+    console.log("resultado", idades[i]);
 }
-while(idade<18);
-    
-    
-   
-    
-
-
-//for: Soma
-//while inquanto for verdadeiro
