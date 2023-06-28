45615d9cf89d7275f78c6d0c6779a9be481b21f3
Lucas Correa <lucascorreanasarioprogramador@gmail.com>
2023-06-28 17:37:27 -0300

    Feat: atividade simples parea entregar numeros impares

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// resto %
-// elevado *
+const quantidade = 8;
 
-console.log(7**2);
-console.log(100%20);
+function Impares(valor){
+    let contar = 0;
+    let newValor = valor
+
+    while(contar<quantidade){
+        if(newValor % 2 !== 0){
+            console.log(newValor);
+            contar++;
+        }
+    newValor++;
+    }
+    
+}
+
+Impares(34);
