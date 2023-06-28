16c45717321954d424d90e6302c5d5d29b45a518
Lucas Correa <lucascorreanasarioprogramador@gmail.com>
2023-06-27 22:01:52 -0300

    feat: operador: resto e elevado

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const consumoCar = 12;
+// resto %
+// elevado *
 
-function gastoGasolina(tempoGasto, velocidadeMedia) {
-    return (tempoGasto * velocidadeMedia) / consumoCar;
-}
-
-console.log(gastoGasolina(10, 85).toFixed(3));
-console.log(gastoGasolina(2, 92).toFixed(3));
-console.log(gastoGasolina(22, 67).toFixed(3));
+console.log(7**2);
+console.log(100%20);
